036b5116a5d1ce079b5247f0e62f2c8b65c24f8a
Leekas Shep <leekasshep@gmail.com>
2015-05-24 14:41:47 +0530

    Removed async done call.

--- test/test-app.js
+++ test/test-app.js
@@ -7,14 +7,14 @@ var os = require('os');
 
 describe('opencart:app', function () {
 
-    before(function (done) {
+    before(function () {
         helpers.run(path.join(__dirname, '../generators/app'))
             .withOptions({skipInstall: true})
             .withPrompts({opencartVersion: '2.0.2.0'})
             .on('end', done);
     });
 
-    it('creates project tempalate files', function (done) {
+    it('creates project tempalate files', function () {
         assert.file([
             '.editorconfig',
             '.jshintrc'
